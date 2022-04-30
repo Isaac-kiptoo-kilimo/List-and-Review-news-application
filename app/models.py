3d4110e50b0bdca799154bf552ec2baa73681ce3
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-30 21:13:12 +0300

    feature:added fuctions to model class

--- app/models.py
+++ app/models.py
@@ -1,24 +1,56 @@
+from pydoc import cli
+
+
 class Article:
 
-    def __init__(self,id_,name_,author,title,description_,url_,published_at,content):
+    articles_list=[]
+
+    def __init__(self,id_,name_,author,title,image_url,description_,url_,published_at,content):
         self.id_ =id_
         self.name_ = name_
         self.author = author
         self.title = title
-        self.url_ = " "https://www.theverge.com/2022/4/8/23016553/block-blockstream-tesla-solar-bitcoin-mine-texas"," + poster
+        self.image_url=image_url
         self.description_= description_
+        self.url_ =url_
         self.published_at = published_at
         self.content = content
+    
+    @classmethod
+    def save_article(self):
+        Article.articles_list.append(self)
+    
+    @classmethod
+    def clear_articles(cls):
+        Article.articles_list.clear()
+
+    @classmethod
+    def get_articles(cls):
+        return cls.articles_list
+
 
         
 class Source:
+
+    sources_list=[]
 
     def __init__(self,id,name,description,url,category,language,country):
         self.id =id
         self.name = name
         self.description= description
-        self.url = "https://abcnews.go.com"," + poster
-        
+        self.url =url 
         self.category = category
         self.language = language
         self.country = country
+
+    @classmethod
+    def save_source(self):
+        Source.sources_list.append(self)
+    
+    @classmethod
+    def clear_sources(cls):
+        Source.sources_list.clear()
+
+    @classmethod
+    def get_sources(cls):
+        return cls.sources_list
