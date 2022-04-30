25f57af337883f3a952c959e318f5946e503085a
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-30 20:48:30 +0300

    feature:rendering models

---
+++ app/models.py
@@ -0,0 +1,24 @@
+class Article:
+
+    def __init__(self,id_,name_,author,title,description_,url_,published_at,content):
+        self.id_ =id_
+        self.name_ = name_
+        self.author = author
+        self.title = title
+        self.url_ = " "https://www.theverge.com/2022/4/8/23016553/block-blockstream-tesla-solar-bitcoin-mine-texas"," + poster
+        self.description_= description_
+        self.published_at = published_at
+        self.content = content
+
+        
+class Source:
+
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id =id
+        self.name = name
+        self.description= description
+        self.url = "https://abcnews.go.com"," + poster
+        
+        self.category = category
+        self.language = language
+        self.country = country
