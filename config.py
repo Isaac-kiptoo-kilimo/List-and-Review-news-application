d1abdf4ea12dbbed2fa481ab5156571a6d9370ba
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-01 21:10:33 +0300

    fix:fixing the 404 error

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-# from distutils.debug import DEBUG
+
 import os
 
 
 class Config:
-    API_KEY='cd75cc1a141e4844980dd4f4bc2fc377'
+    API_KEY='1b0f2bd179f44636bef9b3fe9da611dd'
     BASE_URL='https://newsapi.org/v2/everything?apiKey={}'
     SOURCES_URL='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
     SPECIFIC_SOURCE_ARTICLES_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
