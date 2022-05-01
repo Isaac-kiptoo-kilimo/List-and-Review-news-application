45ba8a71593443aa3047175e0d53b513d63bd2ff
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-01 10:31:08 +0300

    feature:added the prodconfig class and Deveconfig class

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
+from distutils.debug import DEBUG
 import os
+
 
 class Config:
     API_KEY='cd75cc1a141e4844980dd4f4bc2fc377'
     BASE_URL='https://newsapi.org/v2/everything?apiKey={}'
     SOURCES_URL='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
     SPECIFIC_SOURCE_ARTICLES_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+class ProdConfig(Config):
+    DEBUG=False
+
+class DevConfig(Config):
+    DEBUG=True
+
+config_options={
+    'produnction':ProdConfig,
+    'development':DevConfig
+}
