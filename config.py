75f4bbf0935e4d527bf9797eeaa788a0139dc9d6
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-01 10:26:42 +0300

    feature:added the config class

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    API_KEY='cd75cc1a141e4844980dd4f4bc2fc377'
+    BASE_URL='https://newsapi.org/v2/everything?apiKey={}'
+    SOURCES_URL='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    SPECIFIC_SOURCE_ARTICLES_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
