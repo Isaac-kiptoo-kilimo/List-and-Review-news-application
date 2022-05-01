ec318bd8c24e4ee55f85993e7f24041708e8f9d7
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-01 10:05:33 +0300

    feature:get articles based on source function

--- app/requests.py
+++ app/requests.py
@@ -33,7 +33,7 @@ def get_sources():
     return sources_results
 
 
-def process_sources(movie_list):
+def process_sources(sources_list):
     '''
     Function  that processes the movie result and transform them to a list of Objects
     Args:
@@ -56,26 +56,20 @@ def process_sources(movie_list):
         sources.append(source)
     
     return sources 
-def get_movie(id):
-    get_movie_details_url = base_url.format(id,api_key)
 
-    with urllib.request.urlopen(get_movie_details_url) as url:
-        movie_details_data = url.read()
-        movie_details_response = json.loads(movie_details_data)
+def get_articles_based_on_source(source_id):
+    url_ = base_url.format(source_id, api_key)
 
-        movie_object = None
-        if movie_details_response:
-            id = movie_details_response.get('id')
-            title = movie_details_response.get('original_title')
-            overview = movie_details_response.get('overview')
-            poster = movie_details_response.get('poster_path')
-            vote_average = movie_details_response.get('vote_average')
-            vote_count = movie_details_response.get('vote_count')
+    with urllib.request.urlopen(url_) as url:
+        data_ = url.read()
+        response_ = json.loads(data)
 
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+        articles = None
+        if response_:
+            print(response_)
 
-    return movie_object
-
+    return articles
+    
 def search_movie(movie_name):
     search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
     with urllib.request.urlopen(search_movie_url) as url:
