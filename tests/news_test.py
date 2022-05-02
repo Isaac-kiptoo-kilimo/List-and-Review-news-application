43b8b5f3a177623ce6c35ce0834c7e296bead3b8
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-02 18:27:17 +0300

    feature:Deploying news api application

--- tests/news_test.py
+++ tests/news_test.py
@@ -10,8 +10,21 @@ class ArticleTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_article =Article(1234,'Python Must Be Crazy','A thrilling new Python Series','A thrilling new Python Series','/khsjha27hbs','News', 'analysis from the Middle East' ,8.5,129993)
+        self.new_article =Article('abc','mashirima kapombe','Python Must Be Crazy','https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80','A thrilling new Python Series','https://newsapi.org/v2/top-headlines/sources',8-5-2015, 'analysis from the Middle East' ,)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Article))
+
+#     def test_init(self):
+#         self.assertEqual(self.new_article.source,'abc')
+#         self.assertEqual(self.new_article.author,'mashirima kapombe')
+#         self.assertEqual(self.new_article.title,'Python Must Be Crazy')
+#         self.assertEqual(self.new_article.image_url,'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80')
+#         self.assertEqual(self.new_article.description_,'Python Must Be Crazy')
+#         self.assertEqual(self.new_article.url_,'https://newsapi.org/v2/top-headlines/sources')
+#         self.assertEqual(self.new_article.published_at ,8-5-2015)
+#         self.assertEqual(self.new_article.content,'analysis from the Middle East')
+
+# if __name__=='__main__':
+#     unittest.main()
 
